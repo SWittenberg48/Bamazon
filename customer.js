13d9b385d9b8cbb1dc05c710ec2a7f656246e2cd
Scott wittenberg <wittenb48@gmail.com>
2019-04-01 09:49:28 -0500

    Deleted Local Node_modules

--- customer.js
+++ customer.js
@@ -13,3 +13,82 @@ connection.connect(function(err) {
   console.log("connected as id:" + connection.threadId);
   start();
 });
+
+var start = function() {
+  inquirer
+    .prompt({
+      name: "postOrBid",
+      type: "rawlist",
+      message: "Would you like to [POST] or [BID]",
+      choices: ["POST", "BID"]
+    })
+    .then(function(answer) {
+      if (answer.postOrBid.toUpperCase() == "POST") {
+        postAuction();
+      } else {
+        bidAuction();
+      }
+    });
+};
+var bidAuction = function() {
+  connection.query("SELECT * FROM auctions", function(err, res) {
+    console.log(res);
+    inquirer
+      .prompt({
+        names: "choice",
+        type: "rawlist",
+        choices: function(value) {
+          var choiceArray = [];
+          for (var i = 0; i < res.length; i++) {
+            choiceArray.push(res[i].product_name);
+          }
+          return choiceArray;
+        },
+        message: "What Item would you like to place a bid on?"
+      })
+      .then(function(answer) {
+        for (let i = 0; i < res.length; i++) {
+          if (res[i].product_name == answer.choices) {
+            var chosenItem = res[i];
+            inquirer
+              .prompt([
+                {
+                  name: "bid",
+                  type: "input",
+                  message: "How much would you like to bid?",
+                  validate: function(value) {
+                    if (isNaN(value) == false) {
+                      return true;
+                    } else {
+                      return false;
+                    }
+                  }
+                }
+              ])
+              .then(function(answer) {
+                if (chosenItem.highestbid < parseInt(answer.bid)) {
+                  connection.query(
+                    "UPDATE auctions SET ? WHERE ?",
+                    [
+                      {
+                        highestbid: answer.bid
+                      },
+                      {
+                        id: chosenItem.id
+                      }
+                    ],
+                    function(err, res) {
+                      console.log("Bid successfully Placed");
+                      start();
+                    }
+                  );
+                } else {
+                  console.log("Your bid was too low. Try again...");
+                  start();
+                }
+              });
+          }
+        }
+      });
+  });
+};
